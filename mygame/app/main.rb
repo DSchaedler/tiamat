4d1ec3728b7e37ff9130d1bb077bd27f7a5f933e
Dee Schaedler <dominicschaedler@gmail.com>
2023-02-21 10:40:26 -0500

    update rendering to utilize render_targets

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 def tick(args)
-  default_canvas ||= Canvas.new(100, 100, :default_canvas)
+
+  args.state.default_canvas ||= Canvas.new(100, 100, :default_canvas, args)
   
-  args.outputs.primitives << default_canvas.pixels
+  args.render_target(:default_render_target).clear_before_render = false
+  args.outputs.sprites << {x: 100, y: 100, w: 100, h: 100, path: :default_render_target, source_x: 0, source_y: 0, source_w: 100, source_h: 100}
 
 end
 
 class Canvas
   attr_accessor :width, :height, :name, :pixels
 
-  def initialize(width, height, name)
+  def initialize(width, height, name, args)
     @width = width
     @height = height
     @name = name
@@ -24,7 +26,8 @@ class Canvas
       iter_y = @height
 
       while iter_y > 0
-        @pixels[iter_x][iter_y] ||= {primitive_marker: :solid, x: iter_x, y: iter_y, w: 1, h: 1, r: iter_x, g: iter_y, b: 0, a: 255}
+        @pixels[iter_x][iter_y] = {x: iter_x, y: iter_y, w: 1, h: 1, r: iter_x, g: iter_y, b: 0, a: 255, primitive_marker: :solid}
+        args.render_target(:default_render_target).primitives << @pixels[iter_x][iter_y]
 
         iter_y -= 1
       end
